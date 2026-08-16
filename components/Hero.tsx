6725d28f57ca171c4f2609b79b6093db79c47e48
theju <thesigany@gmail.com>
2026-08-17 00:59:31 +0530

    Update Hero component text to reflect current availability

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,11 +2,14 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section id='hero' className='min-h-[calc(100vh-80px)] md:min-h-screen max-sm:pt-20 flex items-center'>
+    <section
+      id='hero'
+      className='min-h-[calc(100vh-80px)] md:min-h-screen max-sm:pt-20 flex items-center'
+    >
       <div className='flex flex-col md:flex-row items-center justify-between w-full gap-12 px-10'>
         <div className='flex-1 px-6 md:px-12 lg:pl-50'>
           <p className='animate-fade-up delay-1 font-mono text-sm font-medium mb-6 ml-2 text-accent tracking-wide'>
-            Available for remote work
+            Open to opportunities
           </p>
 
           <h1 className='font-heading text-5xl md:text-7xl lg:text-9xl font-bold animate-fade-up delay-2 text-text tracking-tight mb-8 uppercase'>
